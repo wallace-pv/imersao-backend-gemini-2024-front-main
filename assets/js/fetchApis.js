56b552b17fd65965c1a4b4bec5bd31d8e3ee9d33
wallace-pv <wallace-pv@hotmail.com>
2024-12-01 16:41:46 -0300

    api url

--- assets/js/fetchApis.js
+++ assets/js/fetchApis.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 // Função para buscar os dados do endpoint
 export default async function fetchImages() {
   try {
-    const response = await fetch(process.env.API_URL); // Usando a URL importada
+    const response = await fetch("https://insta-back-1-1060771770744.southamerica-east1.run.app/posts"); // Usando a URL importada
     const data = await response.json();
     return data;
   } catch (error) {
